ff2c832794e86a75dd15e8f9b38c2890c70a1bc7
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-05-27 18:44:23 +0200

    Add project files.

---
+++ ShoeExercise/Duck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeExercise
+{
+    public class Duck :IComparable<Duck>
+    {
+       public int Size;
+       public KindOfDuck Kind;
+
+        public int CompareTo(Duck other)
+        {
+            if(this.Size > other.Size)
+                return 1;
+            else if(this.Size < other.Size)
+            return -1;
+            else 
+                return 0;
+        }
+    }
+    public enum KindOfDuck
+    {
+        Decoy,
+        Muscovy,
+        Mallard,
+    }
+    public class DuckComparerBySize : IComparer<Duck>
+    {
+        public int Compare(Duck x,Duck y)
+        {
+            if(x.Size < y.Size)
+                return -1;
+            if(x.Size > y.Size)
+                return 1;
+            return 0; 
+        }
+    }
+    public class DuckComparerByKind : IComparer<Duck>
+    {
+        // porównuje wg. indexów typów wyliczeniowych (enum)
+        public int Compare(Duck x,Duck y)
+        {
+            if(x.Kind < y.Kind)
+                return -1;
+            if(x.Kind> y.Kind)
+                return 1;
+            return 0; 
+        }
+    }
+}
---
+++ ShoeExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using ShoeExercise;
+
+static void PrintDucks(List<Duck> ducks)
+{
+    foreach (Duck duck in ducks)
+    {
+        Console.WriteLine(duck.Kind.ToString() + " " + duck.Size.ToString() + "-centymetrowa kaczka ");
+    }
+    Console.WriteLine("\n");
+}
+List<Duck> ducks = new List<Duck>()
+{
+    new Duck(){ Kind = KindOfDuck.Decoy,Size = 17},
+    new Duck(){ Kind = KindOfDuck.Muscovy,Size = 12},
+    new Duck(){ Kind = KindOfDuck.Mallard,Size = 15},
+    new Duck(){ Kind = KindOfDuck.Muscovy,Size = 19},
+    new Duck(){ Kind = KindOfDuck.Decoy,Size = 18},
+    new Duck(){ Kind = KindOfDuck.Mallard,Size = 20},
+};
+//ducks.Sort();
+PrintDucks(ducks);
+
+DuckComparerBySize sizeComparer = new DuckComparerBySize();
+//ducks.Sort(sizeComparer);
+//PrintDucks(ducks);
+
+DuckComparerByKind kindComparer = new DuckComparerByKind();
+//ducks.Sort(kindComparer);
+//PrintDucks(ducks);
+
+//utworzenie instancji porównującej, potem wybranie jak sortować
+DuckComparer comparerDuck = new DuckComparer();
+comparerDuck.SortBy = SortCriteria.SizeThenKind;
+ducks.Sort(comparerDuck);
+PrintDucks(ducks);
+
+comparerDuck.SortBy = SortCriteria.KindThenSize;
+ducks.Sort(comparerDuck);
+PrintDucks(ducks);
+
+Console.ReadKey();
+
